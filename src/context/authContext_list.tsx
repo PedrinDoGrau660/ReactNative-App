ae70106e6c0fee01f33525015b2f5bbcdee54945
Pedro Henrique Hahn Latoch <pedro.latoch@gmail.com>
2025-09-23 08:44:30 -0300

    arrumando modal, aplicando style dentro do propio arquivo

--- src/context/authContext_list.tsx
+++ src/context/authContext_list.tsx
@@ -1,18 +1,65 @@
-import React, { createContext, useContext } from "react";
-import { Alert } from "react-native";
+import React, { createContext, useContext, useRef } from "react";
+import { Dimensions,StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { MaterialIcons, AntDesign} from '@expo/vector-icons'
+import { Modalize } from "react-native-modalize";
+import { style } from "../pages/login/styles";
+
 
 export const AuthContextList: any = createContext({});
 
-export const AuthProviderList = ({ children }: any) => {
-  const onOpen = () => {
-    Alert.alert("Atenção", "Modal aberto!");
-  };
+export const AuthProviderList = (props: any): any => {
 
-  return (
-    <AuthContextList.Provider value={{ onOpen }}>
-      {children}
-    </AuthContextList.Provider>
-  );
-};
+    const modalizeRef = useRef<Modalize>(null);
+    const onOpen = () => {
+        modalizeRef?.current?.open();
+
+    }
+    const _container = () => {
+        return (
+          <View style={Styles.container}>
+            <View style={Styles.header}>
+              <TouchableOpacity>
+                <MaterialIcons
+                  name="close"
+                  size={24}
+                />
+                </TouchableOpacity>
+                <Text>Criar Tarefa</Text>
+
+                <TouchableOpacity>
+                <AntDesign
+                  name="check"
+                  size={30}
+                />
+                </TouchableOpacity>
+            </View>
+          </View>
+        )
+
+    }
+    return (
+        <AuthContextList.Provider value={{ onOpen }}>
+            {props.children}
+            <Modalize
+                ref={modalizeRef}
+               // modalHeight={Dimensions.get('window').height / 1.3}
+               childrenStyle={{height: Dimensions.get('window').height / 1.3}}
+               adjustToContentHeight={true}
+            >
+            {_container()}
+            </Modalize>
+        </AuthContextList.Provider>
+    )
+}
 
 export const useAuth = () => useContext(AuthContextList);
+
+export const Styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+  })
