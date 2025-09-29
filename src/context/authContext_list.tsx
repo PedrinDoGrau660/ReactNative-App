d02a557a847f17ba41cffaf6f48f6d6f96bdf4db
Pedro Henrique Hahn Latoch <pedro.latoch@gmail.com>
2025-09-29 14:18:41 -0300

    Criando a funcionalidade para data e gora para gerenciar

--- src/context/authContext_list.tsx
+++ src/context/authContext_list.tsx
@@ -23,6 +23,8 @@ export const AuthProviderList = (props: any): any => {
     const [selectedFlag, setSelected] = useState('Urgente');
     const [selectedDate, setSelectedDate] = useState(new Date());
     const [selectedTime, setSelectedTime] = useState(new Date());
+    const [showDatePicker, setShowDatePicker] = useState(false);
+    const [showTimePicker, setShowTimePicker] = useState(false);
 
 
     const onOpen = () => {
@@ -50,6 +52,11 @@ export const AuthProviderList = (props: any): any => {
             ))
         )
     }
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    }
+    const handleTimeChange = (time) => {
+        setSelectedTime(time);
 
     const _container = () => {
         return (
@@ -99,9 +106,15 @@ export const AuthProviderList = (props: any): any => {
                         labelStyle={styles.label}
                     /> */}
                     <CustomDateTimePicker
-                        onDateChange={() => {}}
-                        setShow={() => {}}
-                        show={true}
+                        onDateChange={handleDateChange}
+                        setShow={setShowDatePicker}
+                        show={showDatePicker}
+                        type={'date'}
+                    />
+                    <CustomDateTimePicker
+                        onDateChange={handleTimeChange}
+                        setShow={setShowTimePicker}
+                        show={showTimePicker}
                         type={'date'}
                     />
                 </View>
